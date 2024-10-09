eb6960c2da5ce1f291320368b2c68257246a4282
mvlexs <98380845+mvlexs@users.noreply.github.com>
2024-10-09 17:46:41 +0200

    implemented proper displaying of images in widget via bitmap,finalized the in-app display of data pulled from server

--- app/src/sharedTest/java/com/example/jetnews/TestAppContainer.kt
+++ app/src/sharedTest/java/com/example/jetnews/TestAppContainer.kt
@@ -21,12 +21,12 @@ import com.example.jetnews.data.AppContainer
 import com.example.jetnews.data.interests.InterestsRepository
 import com.example.jetnews.data.interests.impl.FakeInterestsRepository
 import com.example.jetnews.data.posts.PostsRepository
-import com.example.jetnews.data.posts.impl.BlockingFakePostsRepository
+import com.example.jetnews.data.posts.impl.HttpPostsRepository
 
 class TestAppContainer(private val context: Context) : AppContainer {
 
     override val postsRepository: PostsRepository by lazy {
-        BlockingFakePostsRepository()
+        HttpPostsRepository()
     }
 
     override val interestsRepository: InterestsRepository by lazy {
