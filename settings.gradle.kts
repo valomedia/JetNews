b5a2325a42b41ac21d56b6edf98c4b6233d0d0e6
Jean-Pierre Höhmann <jean-pierre@hoehmann.jp>
2025-11-18 23:48:20 +0100

    Suppress some warnings about unstable API usage.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,9 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
     repositories {
         snapshotVersion?.let {
             println("https://androidx.dev/snapshots/builds/$it/artifacts/repository/")
