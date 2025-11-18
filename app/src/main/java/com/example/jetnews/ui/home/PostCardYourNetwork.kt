7df27ad8c3c822108a7d4bf0c16d1a5f91f547ec
Jean-Pierre Höhmann <jean-pierre@hoehmann.jp>
2025-11-18 23:48:37 +0100

    Clean up unused imports.

--- app/src/main/java/com/example/jetnews/ui/home/PostCardYourNetwork.kt
+++ app/src/main/java/com/example/jetnews/ui/home/PostCardYourNetwork.kt
@@ -18,7 +18,6 @@
 package com.example.jetnews.ui.home
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -33,7 +32,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
